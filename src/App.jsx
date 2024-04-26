594297f91dec313ba07a831698d078c5ff1d5fcf
jatinkhatta <jatinkhatta55@gmail.com>
2024-04-26 14:07:31 +0530

    added localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,17 @@ const average = (arr) =>
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  //const [watched, setWatched] = useState();
+  const [watched, setWatched] = useState(function () {
+    const storedValue = localStorage.getItem("watch");
+    if (!storedValue) return [];
+    else return JSON.parse(storedValue);
+  });
+
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
   }
@@ -20,11 +27,18 @@ export default function App() {
   }
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
+
+    //localStorage.setItem("watched", JSON.stringify([...watched, movie]);
   }
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
-
+  useEffect(
+    function () {
+      localStorage.setItem("watch", JSON.stringify(watched));
+    },
+    [watched]
+  );
   useEffect(
     function () {
       const controller = new AbortController();
